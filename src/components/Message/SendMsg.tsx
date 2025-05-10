877d8aaefe1f938d685378bd25df9d5103ac95eb
Harsh Agarwal <harshag.code@gmail.com>
2025-05-10 09:02:45 +0530

    scroll-y

--- src/components/Message/SendMsg.tsx
+++ src/components/Message/SendMsg.tsx
@@ -3,11 +3,12 @@ import { IoIosReverseCamera } from "react-icons/io";
 import { FaMicrophoneLines } from "react-icons/fa6";
 import { FaFaceSmile } from "react-icons/fa6";
 import EmojiPicker from 'emoji-picker-react'
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function SendMsg() {
     const [text, setText] = useState('');
     const [emojiState, setEmojiState] = useState(false);
+    const inputRef = useRef(null);
 
     function textChange(value: string) {
         setText(value);
@@ -15,7 +16,17 @@ function SendMsg() {
 
     function textWithEmoji(emoji: string) {
         setText(prev => prev+emoji);
+        inputRef.current?.focus();
     }
+
+    useEffect(() => {
+        function escapePress(e: KeyboardEvent) {
+            if (e.key == 'Escape') {
+                setEmojiState(false);
+            }
+        }
+        window.addEventListener('keydown', escapePress);
+    }, [])
 
     return (
         <div className="relative flex flex-row justify-between gap-2 items-center py-3 px-2 border-t-[1px] border-gray-400">
@@ -36,7 +47,7 @@ function SendMsg() {
                 <button onClick={() => setEmojiState(!emojiState)}><FaFaceSmile className="text-white font-bold absolute text-base top-[50%] translate-y-[-50%] mx-2" /></button>
 
 
-                <input type="text" name="sendMsg" id="sendMsg" className=" text-sm px-2 ps-8 py-1 rounded-md outline-none bg-slate-800 border-[1px] border-gray-500 text-gray-300 w-64 tracking-wider " placeholder="Type a message..." value={text} onChange={(e) => textChange(e.target.value)} />
+                <input ref={inputRef} type="text" name="sendMsg" id="sendMsg" className=" text-sm px-2 ps-8 py-1 rounded-md outline-none bg-slate-800 border-[1px] border-gray-500 text-gray-300 w-64 tracking-wider " placeholder="Type a message..." value={text} onChange={(e) => textChange(e.target.value)} />
             </div>
             <div>
                 <button className=" text-white bg-gray-400 text-sm py-1 px-2 rounded-md hover:bg-gray-500">Send</button>
